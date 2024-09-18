83ecabd420c5056a7d191faec393bfdf2d3a6dfd
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-18 23:51:04 +0600

    access value

---
+++ js/home.js
@@ -0,0 +1,12 @@
+document.getElementById('add-button').addEventListener('click', function(event){
+    event.preventDefault();
+    const yourPin = document.getElementById('your-pin').value;
+    const yourNumber = document.getElementById('your-number').value;
+    const yourBalance = document.getElementById('available-balance').value;
+    // if(yourPin === '1234'){
+    //     yourNumber += yourBalance
+    //     return yourNumber;
+    // }else{
+    //     alert('er parlam na mamu')
+    // }
+})
--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
+// document.getElementById('login-btn').addEventListener('click', function(event){
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('your-pin').value;
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('I am Okey Please Click Next Button');
+//     }
+//     else {
+//         alert('please input right number or pin')
+//     }
+// })
+
 document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('oi mama na pls....')
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('your-pin').value;
+    if (phoneNumber ===  '017' && pinNumber === '1234'){
+        window.location.href = './home.html'
+        console.log('Oi Mama Na pls.....');
+    }
+    else {
+        alert('please enter right phone number or pin')
+    }
 })
