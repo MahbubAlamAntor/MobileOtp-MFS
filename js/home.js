2416156c9563b9b90a4d735cba5f2eb36e318e3b
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-19 00:21:16 +0600

    pin matched then work balance add

--- js/home.js
+++ js/home.js
@@ -2,11 +2,19 @@ document.getElementById('add-button').addEventListener('click', function(event){
     event.preventDefault();
     const yourPin = document.getElementById('your-pin').value;
     const yourNumber = document.getElementById('your-number').value;
-    const yourBalance = document.getElementById('available-balance').value;
-    // if(yourPin === '1234'){
-    //     yourNumber += yourBalance
-    //     return yourNumber;
-    // }else{
-    //     alert('er parlam na mamu')
-    // }
+    // console.log(yourNumber)
+    if(yourPin === '1234' && yourNumber > 20){
+        const yourBalance = document.getElementById('available-balance').innerText;
+        
+        const yourNumbers = parseFloat(yourNumber);
+        const yourBalances = parseFloat(yourBalance)
+        
+        const newNumber = yourNumbers + yourBalances;
+        
+        document.getElementById('available-balance').innerText = newNumber;
+        
+    }
+    else{
+        alert('er parlam na mamu')
+    }
 })
